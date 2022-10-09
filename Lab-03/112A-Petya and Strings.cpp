9f79c31a9d9e83f9f769ec7a1f3c17a52436f6a0
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2022-10-09 22:19:11 +0600

    Create 112A-Petya and Strings.cpp

---
+++ Lab-03/112A-Petya and Strings.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a,b;
+    int i;
+    cin>>a>>b;
+   for( i=0;i<a.size();i++)
+   {     a[i]=tolower(a[i]);
+         b[i]=tolower(b[i]);
+   }
+           if(a==b)
+           {
+               cout<<0<<endl;
+           }
+           else
+           {
+               for(i=0;a.size();i++)
+               {
+                   if(a[i]<b[i])
+                   {
+                       cout<<-1<<endl;
+                       break;
+                   }
+                   else if(a[i]>b[i])
+                   {
+                       cout<<1<<endl;
+                       break;
+                   }
+               }
+           }
+           return 0;
+}
