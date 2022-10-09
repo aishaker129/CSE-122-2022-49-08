a1f87e21cfdb8c79b7f2af67c4af19a2fea3ac07
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2022-10-09 22:13:00 +0600

    Create 5.356-49-08.cpp

---
+++ Lab-02/5.356-49-08.cpp
@@ -0,0 +1,11 @@
+// problem name-Domino piling
+// problem no-50A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    cout << (n*m)/2 << endl;
+    return 0;
+}
