8be7fe439cf2ed046755d14a68e394b4061fa98b
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2022-10-09 22:02:35 +0600

    Create 1.356-49-08.cpp

---
+++ Lab-01/1.356-49-08.cpp
@@ -0,0 +1,16 @@
+//problem name-Watermelon
+//problem no-4A
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long w;
+    cin>>w;
+    if(w%2==0 && w>2)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+        return 0;
+
+
+}
